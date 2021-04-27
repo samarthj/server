89bd8eb93e5c2800bd290a2ea5b2aad42da3644e
Julius Goryavsky <julius.goryavsky@mariadb.com>
2021-04-27 04:27:27 +0200

    MDEV-24962: Galera SST innobackupex-move ignores Environment settings
    After switching to the new mariabackup interface (instead of
    the outdated innobackupex interface, which is supported for
    compatibility), we need to explicitly pass a path to the datadir
    directory as a parameter, since in the new interface the value
    of this option is not automatically set in such a way that it
    always matches the SST/IST logic. This commit adds passing this
    option as an explicit parameter to mariabackup. This commit also
    removed unnecessary options that are not used and not supported
    by mariabackup.
    
    Also, numerous flaws in the common wsrep_sst_common script have
    been fixed:
    
     1) There are many bash-specific constructs in the script that
        may not be supported by other interpreters, which can lead
        to the most unexpected errors during SST, because failures
        in the interpretation of bash-specific constructs lead to
        incorrect parsing of arguments;
     2) The parse_cnf() function which is often called by other scripts
        for the "mysqld" or "--mysqld" group, but it does not take into
        account the default group suffix. The suffix is not taken into
        account in wsrep_sst_common, and in all other scripts too,
        which leads to reading values only from the default group,
        which then leads to error due to reading the default values
        instead of the values for a specific group;
     3) Some options such as --innodb-data-home-dir and --datadir
        are not removed from the --mysqld-args list, although they are
        processed inside scripts and further double passing of these
        options may cause problems for mariabackup;
     4) If an argument that the script understands is present in
        the --mysqld-args list twice, then this causes SST to fail,
        instead of reading the most recent value;
     5) The "--host" parameter is technically still supported among
        the arguments to mysqld_common, but in reality scripts do not
        work with it as expected, especially if it has an IPv6 address
        in it;
     6) If the port number is absent in the --address parameter, but
        the port is explicitly passed through the --port parameter,
        then the scripts for mariabackup and xtrabackup-v2 fail;
     7) If a new interface is used (with the --address parameter),
        then automatic port substitution is not performed by default,
        although it is supported for the legacy --host/--port
        interface.
     8) If there are spaces in the parameter values after --mysqld_args,
        then their further transfer does not occur correctly, which
        causes mariabackup to fail during SST if the space splits
        the argument in such a way that it breaks the parsing of the
        parameters;
     9) If most of the parameters that are names and paths for files
        and directories contain spaces, then SST scripts fail in an
        unpredictable way due to incorrect variable substitution;
    10) If, among the arguments to mysqld (--mysqld-args), the --log-bin
        option is passed without a parameter, but the --binlog option
        is not passed, then the script cannot substitute the default
        name for binlog and cannot construct it using the --log-basename
        which is against specs;
    11) Tail slashes are not removed from directory names, which, upon
        further substitution, leads to the appearance of a double slash
        in the file paths;
    12) The explicit --binlog parameter (which is now always transmitted
        from the server side) and the "hidden" --log-bin parameter in the
        list of options after --mysqld-args are perceived as two different
        parameters in two different parts of the scripts, and if they are
        do not match for some reason, it will lead to failures during SST;

--- scripts/wsrep_sst_xtrabackup.sh
+++ scripts/wsrep_sst_xtrabackup.sh
@@ -18,10 +18,8 @@
 # Optional dependencies and options documented here: http://www.percona.com/doc/percona-xtradb-cluster/manual/xtrabackup_sst.html 
 # Make sure to read that before proceeding!
 
-
-
-
 . $(dirname $0)/wsrep_sst_common
+wsrep_check_datadir
 
 ealgo=""
 ekey=""
@@ -134,7 +132,7 @@ get_keys()
 
 get_transfer()
 {
-    TSST_PORT=${WSREP_SST_OPT_PORT:-4444}
+    TSST_PORT=$WSREP_SST_OPT_PORT
 
     if [[ $tfmt == 'nc' ]];then
         if [[ ! -x `which nc` ]];then 
@@ -424,14 +422,14 @@ then
         usrst=0
         TMPDIR="${TMPDIR:-/tmp}"
 
-        if [[ -n "${WSREP_SST_OPT_USER:-}" && "$WSREP_SST_OPT_USER" != "(null)" ]]; then
+        if [ -n "$WSREP_SST_OPT_USER" -a "$WSREP_SST_OPT_USER" != '(null)' ]; then
            INNOEXTRA+=" --user=$WSREP_SST_OPT_USER"
            usrst=1
         fi
 
-        if [ -n "${WSREP_SST_OPT_PSWD:-}" ]; then
+        if [ -n "$WSREP_SST_OPT_PSWD" ]; then
            INNOEXTRA+=" --password=$WSREP_SST_OPT_PSWD"
-        elif [[ $usrst -eq 1 ]];then
+        elif [ $usrst -eq 1 ]; then
            # Empty password, used for testing, debugging etc.
            INNOEXTRA+=" --password="
         fi
@@ -532,9 +530,9 @@ then
     # May need xtrabackup_checkpoints later on
     rm -f ${DATA}/xtrabackup_binary ${DATA}/xtrabackup_galera_info  ${DATA}/xtrabackup_logfile
 
-    ADDR="${WSREP_SST_OPT_HOST}:${WSREP_SST_OPT_PORT:-4444}"
+    ADDR="${WSREP_SST_OPT_HOST}:${WSREP_SST_OPT_PORT}"
 
-    wait_for_listen ${WSREP_SST_OPT_PORT:-4444} ${ADDR} ${MODULE} &
+    wait_for_listen ${WSREP_SST_OPT_PORT} ${ADDR} ${MODULE} &
 
     trap sig_joiner_cleanup HUP PIPE INT TERM
     trap cleanup_joiner EXIT
